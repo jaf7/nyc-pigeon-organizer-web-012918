0c2f7d94dac738ccf08a6a26440fa6dd7d4b3dd5
jaf7 <janthonyfields@gmail.com>
2018-01-20 20:24:07 -0500

    Built reorganized nested data structure from original

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_pigeons = {}
+
+  data.each do |attr_key, attr_hash|
+    # :color, {:purple => ...}
+    # binding.pry
+    attr_hash.each do |attribute, names_array|
+      # :purple, ["Theo", "Peter Jr.", "Lucky"]
+      # binding.pry
+      names_array.each do |name|
+        # binding.pry
+        organized_pigeons[name] ||= {}
+        organized_pigeons[name][attr_key] ||= []
+        organized_pigeons[name][attr_key] << attribute.to_s
+      end
+    end
+  end
+  # binding.pry
+  organized_pigeons
 end
